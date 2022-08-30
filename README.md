0472f1f770cbc01a688adf89c4b57dd378794eca
kabradshaw1 <itllbok82@gmail.com>
2022-08-30 16:22:28 -0400

    Timer working

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Work Day Scheduler 
+
+## Usage
+
+You can type in activities into the grey section.  The blue button off to the right is the save button.  When you press that button, it will move the information in the grey box to local storage, where it will presist when you return to the page or refresh the page.  
+
+![alt text](assets/images/screenshot.png)
+
+## Deployed Web Page
+
+https://kabradshaw1.github.io/Work-Day-Scheduler/
+
+## Credit
+
+The bootcamp team provided some starter html and the css from their example. I believe the starter code was just an html 5 with a couple libraries attacked, and a div element added. I don't think I was actually able to figure out what they wanted me to do with 90% of the css, so I mostly used bootstrap and css I created.  
--- assets/js/script.js
+++ assets/js/script.js
@@ -8,17 +8,34 @@ var currentDay = document.querySelector("#currentDay");
 
 var currentDate = moment();
 
-currentDay.textContent = currentDate.format("MMM, DD, YYYY");
+// console.log(currentDate)
+
+var currentHour = currentDate.format("")
+currentDay.textContent = currentDate.format("dddd, MMM DD, YYYY");
 
 // taskEl = document.querySelector("#task");
 
 auditTask = function() {
-    taskEl = document.querySelector("#task");
-    console.log(taskEl)
-};
+    var pancakes = $("#9");
 
+    var pancakeTime = pancakes.attr("id")
+
+    var time = moment().format("h")
+
+    console.log(pancakeTime)
+    console.log(time)
+    if (pancakeTime > time) {
+        $(pancakes).addClass("past");
+        $(pancakes).removeClass("present")
+        $(pancakes).removeClass("future")
+    } 
+       
+};
 $("button").on("click", function() {
-    console.log(this)
+    var btnClicked = $(target);
+    console.log(btnClicked.parent("<h2>"))
+
+    // console.log(this)
 
 });
 
